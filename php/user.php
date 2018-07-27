cef04ecb4697052131b1b7c4420e16598e623395
rbecker8 <rbecker8@cnm.edu>
2018-07-27 08:41:30 -0600

    Added user class with three data types: userAge, userEmail, userPhone.  Added accessors for each data type.

---
+++ php/user.php
@@ -0,0 +1,54 @@
+<?php
+
+
+class user {
+	/**
+	 * age for User;
+	 * @var string $userAge
+	 **/
+	private $userAge;
+	/**
+	 * email for this Reviewer; this a unique index
+	 * @var string $reviewerEmail
+	 **/
+	private $userEmail;
+	/**
+	 * phone number for this user
+	 * @var string $userPhone
+	 **/
+	private $userPhone;
+
+	/**
+	 * accessor method for user age
+	 *
+	 * @return int value of use age
+	 **/
+	public function getUserAge(): int {
+		return ($this->userAge);
+	}
+
+	/**
+	 * accessor method for user email
+	 *
+	 * @return string value of user email
+	 **/
+	public function getUserEmail(): string {
+		return $this->userEmail;
+	}
+
+
+	/**
+	 * accessor method for user phone
+	 *
+	 * @return string value of phone or null
+	 **/
+	public function getUserPhone(): ?string {
+		return ($this->userPhone);
+	}
+
+
+
+
+
+}
+
